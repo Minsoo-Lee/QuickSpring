2712aee8cb003453d25866f266bda19f8a2ac406
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-19 16:40:17 +0900

    [Fix] Class03 - SamsungTV

--- Day/Day1/src/main/java/polymorphism/SamsungTV.java
+++ Day/Day1/src/main/java/polymorphism/SamsungTV.java
@@ -1,6 +1,15 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
+    public void initMethod() {
+        System.out.println("initializing object...");
+    }
+    public void destroyMethod() {
+        System.out.println("destroying object...");
+    }
+    public SamsungTV() {
+        System.out.println("===> SamsungTV object generated");
+    }
     public void powerOn() {
         System.out.println("SamsungTV --- power on");
     }
