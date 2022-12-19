f627290d1ba7032f2b7ed4807d5b0ac8fe3f3c1b
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-19 17:28:59 +0900

    [Fix] Class04 - Map type mapping

--- Day/Day1/src/main/java/com/springbok/ioc/injection/CollectionBeanClient.java
+++ Day/Day1/src/main/java/com/springbok/ioc/injection/CollectionBeanClient.java
@@ -1,6 +1,6 @@
 package com.springbok.ioc.injection;
 
-import java.util.List;
+import java.util.Set;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -12,7 +12,7 @@ public class CollectionBeanClient {
         AbstractApplicationContext factory =
                 new GenericXmlApplicationContext("applicationContext.xml");
         CollectionBean bean = (CollectionBean)factory.getBean("collectionBean");
-        List<String> addressList = bean.getAddressList();
+        Set<String> addressList = bean.getAddressList();
         for (String address : addressList) {
             System.out.println(address.toString());
         }
