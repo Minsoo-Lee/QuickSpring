0dbbf9ffef76f655633c931516263a1f794b8730
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-21 18:17:08 +0900

    [Fix] Class04 - JoinPoint Method

--- Day/day2/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ Day/day2/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,7 +1,9 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class LogAdvice {
-    public void printLog() {
+    public void printLog(JoinPoint jp) {
         System.out.println("[Common Log] Before Business Logic");
     }
 }
