26433bae5f4406bafb46b638fec7c74bc80a32a3
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-21 17:55:15 +0900

    [Fix] Class03 - After Returning Advice

---
+++ Day/day2/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -0,0 +1,7 @@
+package com.springbook.biz.common;
+
+public class AfterReturningAdvice {
+    public void afterLog() {
+        System.out.println("[AFTER RETURNING] After Business Logic");
+    }
+}
