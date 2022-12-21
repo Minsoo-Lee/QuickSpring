578457e475b768bedd1aed2d62bd5e1102c48c2f
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-21 18:28:45 +0900

    [Fix] Class04 - After Returning Method

--- Day/day2/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ Day/day2/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,7 +1,19 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
+import com.springbook.biz.user.UserVO;
+
 public class AfterReturningAdvice {
-    public void afterLog() {
-        System.out.println("[AFTER RETURNING] After Business Logic");
+    public void afterLog(JoinPoint jp, Object returnObj) {
+        String method = jp.getSignature().getName();
+        if (returnObj instanceof UserVO) {
+            UserVO user = (UserVO) returnObj;
+            if (user.getRole().equals("Admin")) {
+                System.out.println(user.getName() + " Login(Admin)");
+            }
+        }
+
+        System.out.println("[AFTER RETURNING] : " + method + "() Method Return : " + returnObj.toString());
     }
 }
