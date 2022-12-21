ce66b108d4c2adb724a7e5ff923f777a61d86d92
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-21 19:41:32 +0900

    [Fix] Class06 - DAO Class : Register JdbcTemplate <bean>, Dependency Injection

--- Day/day2/src/main/java/com/springbook/biz/board/impl/BoardDAOSpring.java
+++ Day/day2/src/main/java/com/springbook/biz/board/impl/BoardDAOSpring.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
@@ -16,6 +17,8 @@ import com.springbook.biz.board.BoardVO;
 // DAO (Data Access Object)
 @Repository
 public class BoardDAOSpring extends JdbcDaoSupport {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     // SQL 명령어들
     private final String BOARD_INSERT = "insert into board(seq, title, writer, content)" +
             "values((select nvl(max(seq), 0)+1 from board),?,?,?)";
@@ -33,32 +36,32 @@ public class BoardDAOSpring extends JdbcDaoSupport {
     // 글 등록
     public void insertBoard(BoardVO vo) {
         System.out.println("===> insertBoard() by Spring JDBC");
-        getJdbcTemplate().update(BOARD_INSERT, vo.getTitle(), vo.getWriter(), vo.getContent());
+        jdbcTemplate.update(BOARD_INSERT, vo.getTitle(), vo.getWriter(), vo.getContent());
     }
 
     // 글 수정
     public void updateBoard(BoardVO vo) {
         System.out.println("===> updateBoard() by Spring JDBC");
-        getJdbcTemplate().update(BOARD_UPDATE, vo.getTitle(), vo.getContent(), vo.getSeq());
+        jdbcTemplate.update(BOARD_UPDATE, vo.getTitle(), vo.getContent(), vo.getSeq());
     }
 
     // 글 삭제
     public void deleteBoard(BoardVO vo) {
         System.out.println("===> deleteBoard() by Spring JDBC");
-        getJdbcTemplate().update(BOARD_DELETE, vo.getSeq());
+        jdbcTemplate.update(BOARD_DELETE, vo.getSeq());
     }
 
     // 글 상세 조회
     public BoardVO getBoard(BoardVO vo) {
         System.out.println("===> getBoard() by Spring JDBC");
         Object[] args = {vo.getSeq()};
-        return getJdbcTemplate().queryForObject(BOARD_GET, args, new BoardRowMapper());
+        return jdbcTemplate.queryForObject(BOARD_GET, args, new BoardRowMapper());
     }
 
     // 글 목록 조회
     public List<BoardVO> getBoardList(BoardVO vo) {
         System.out.println("===> getBoardList() by Spring JDBC");
-        return getJdbcTemplate().query(BOARD_LIST, new BoardRowMapper());
+        return jdbcTemplate.query(BOARD_LIST, new BoardRowMapper());
     }
 }
 
