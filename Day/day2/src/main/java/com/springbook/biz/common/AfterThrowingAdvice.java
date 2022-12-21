42adcebc4f8f22cf53e79fee2bbb50713613855c
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-21 18:38:57 +0900

    [Fix] Class04 - After Throwing Method

--- Day/day2/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
+++ Day/day2/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
@@ -1,7 +1,18 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class AfterThrowingAdvice {
-    public void exceptionLog() {
-        System.out.println("[AFTER THROWING] Exception While Business Logic");
+    public void exceptionLog(JoinPoint jp, Exception exceptObj) {
+        String method = jp.getSignature().getName();
+        System.out.println(method + "() method cause exception : ");
+
+        if (exceptObj instanceof IllegalArgumentException) {
+            System.out.println("Illegal Arguments Exception");
+        } else if (exceptObj instanceof NumberFormatException) {
+            System.out.println("Number Format Exception");
+        } else if (exceptObj instanceof Exception) {
+            System.out.println("problem exception");
+        }
     }
 }
