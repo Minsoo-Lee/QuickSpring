08f14621356c341c3a0da4c73a48d9eaa10d0ae4
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-12-21 18:22:45 +0900

    [Fix] Class04 - Before Method

--- Day/day2/src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ Day/day2/src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -1,7 +1,12 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class BeforeAdvice {
-    public void beforeLog() {
-        System.out.println("[BEFORE] Before Business Logic");
+    public void beforeLog(JoinPoint jp) {
+        String method = jp.getSignature().getName();
+        Object[] args = jp.getArgs();
+
+        System.out.println("[BEFORE] " + method + "() Method ARGS : " + args[0].toString());
     }
 }
